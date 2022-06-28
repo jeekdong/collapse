d959727c4f708afbbd002b0878cd49cc66dba091
东柯 <jeekdong@gmail.com>
2022-06-28 04:25:03 +0000

    Update 2 files

---
+++ components/collapse.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export const Collapse = () => {
+    const [fold, setFold] = useState(false)
+    
+    return (
+        <div>
+            Collapse
+        </div>
+    )
+}
